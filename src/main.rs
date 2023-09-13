45e223f1ad3acf92ffb55ef9b998154430329731
schluis <luis.scheuch@posteo.de>
2023-09-13 14:16:19 +0200

    add color

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,49 @@
-use image::{Rgb, RgbImage};
+use colors_transform::{Color, Hsl};
+use image::RgbImage;
 use num::{complex::ComplexFloat, Complex};
 use std::time::Instant;
 
-const MAX_RECURSIONS: u32 = 100;
-const WIDTH_PIXEL: u32 = 7500;
-const HEIGHT_PIXEL: u32 = 5000;
+const MAX_RECURSIONS: u32 = 360 * 4;
+const WIDTH_PIXEL: u32 = 1500;
+const HEIGHT_PIXEL: u32 = 1000;
 
-fn mandelbrot(x: f32, y: f32) -> Rgb<u8> {
+fn mandelbrot(x: f32, y: f32) -> u32 {
     let c = Complex::new(x, y);
     let mut z = Complex::new(0.0, 0.0);
 
-    for _recursion_depth in 1..MAX_RECURSIONS {
+    for recursion_depth in 1..MAX_RECURSIONS {
         z = z * z + c;
 
         if z.abs() > 2.0 {
-            return [0, 0, 0].into();
+            return recursion_depth;
         }
     }
-    [255, 255, 255].into()
+    return MAX_RECURSIONS;
 }
 
 fn main() {
     let start = Instant::now();
     let mut image = RgbImage::new(WIDTH_PIXEL, HEIGHT_PIXEL);
 
-    for (x_image, x_math) in (0..WIDTH_PIXEL).map(|x| {
-        (
-            x,
-            (x as f32 - 2.0 / 3.0 * WIDTH_PIXEL as f32) * (3.0 / WIDTH_PIXEL as f32),
-        )
-    }) {
-        for (y_image, y_math) in (0..HEIGHT_PIXEL).map(|y| {
-            (
-                y,
-                (y as f32 - HEIGHT_PIXEL as f32 / 2.0) * (2.0 / HEIGHT_PIXEL as f32),
-            )
-        }) {
-            image.put_pixel(x_image, y_image, mandelbrot(x_math, y_math));
+    for x_image in 0..WIDTH_PIXEL {
+        for y_image in 0..HEIGHT_PIXEL {
+            let x_math =
+                (x_image as f32 - 2.0 / 3.0 * WIDTH_PIXEL as f32) * (3.0 / WIDTH_PIXEL as f32);
+            let y_math = (y_image as f32 - HEIGHT_PIXEL as f32 / 2.0) * (2.0 / HEIGHT_PIXEL as f32);
+
+            let recursions = mandelbrot(x_math, y_math);
+            let rgb = Hsl::from(recursions as f32 % 360.0, 100.0, 50.0);
+
+            image.put_pixel(
+                x_image,
+                y_image,
+                [
+                    rgb.get_red() as u8,
+                    rgb.get_green() as u8,
+                    rgb.get_blue() as u8,
+                ]
+                .into(),
+            );
         }
     }
 
