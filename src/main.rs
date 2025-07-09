68748104af588fcbea3254540101d6e0c7e5017d
schluis <luis.scheuch@posteo.de>
2025-07-09 14:26:55 +0200

    parallelize using rayon, add profile release-with-debug, apply clippy

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use colors_transform::{Color, Hsl};
 use image::RgbImage;
 use num::{complex::ComplexFloat, Complex};
+use rayon::prelude::*;
 use std::time::Instant;
 
 const MAX_RECURSIONS: u32 = 360 * 4;
@@ -18,37 +19,42 @@ fn mandelbrot(x: f32, y: f32) -> u32 {
             return recursion_depth;
         }
     }
-    return MAX_RECURSIONS;
+    MAX_RECURSIONS
 }
 
 fn main() {
     let start = Instant::now();
-    let mut image = RgbImage::new(WIDTH_PIXEL, HEIGHT_PIXEL);
 
-    for x_image in 0..WIDTH_PIXEL {
-        for y_image in 0..HEIGHT_PIXEL {
-            let x_math =
-                (x_image as f32 - 2.0 / 3.0 * WIDTH_PIXEL as f32) * (3.0 / WIDTH_PIXEL as f32);
-            let y_math = (y_image as f32 - HEIGHT_PIXEL as f32 / 2.0) * (2.0 / HEIGHT_PIXEL as f32);
+    let pixels: Vec<[u8; 3]> = (0..HEIGHT_PIXEL)
+        .into_par_iter()
+        .flat_map(|y_image| {
+            (0..WIDTH_PIXEL).into_par_iter().map(move |x_image| {
+                let x_math =
+                    (x_image as f32 - 2.0 / 3.0 * WIDTH_PIXEL as f32) * (3.0 / WIDTH_PIXEL as f32);
+                let y_math =
+                    (y_image as f32 - HEIGHT_PIXEL as f32 / 2.0) * (2.0 / HEIGHT_PIXEL as f32);
 
-            let recursions = mandelbrot(x_math, y_math);
-            let rgb = Hsl::from(recursions as f32 % 360.0, 100.0, 50.0);
+                let recursions = mandelbrot(x_math, y_math);
+                let rgb = Hsl::from(recursions as f32 % 360.0, 100.0, 50.0);
 
-            image.put_pixel(
-                x_image,
-                y_image,
                 [
                     rgb.get_red() as u8,
                     rgb.get_green() as u8,
                     rgb.get_blue() as u8,
                 ]
-                .into(),
-            );
-        }
+            })
+        })
+        .collect();
+
+    let mut image = RgbImage::new(WIDTH_PIXEL, HEIGHT_PIXEL);
+    for (i, pixel) in pixels.iter().enumerate() {
+        let x = (i as u32) % WIDTH_PIXEL;
+        let y = (i as u32) / WIDTH_PIXEL;
+        image.put_pixel(x, y, (*pixel).into());
     }
 
     image.save("mandelbrot_minimal.png").unwrap();
 
     let duration = start.elapsed();
-    println!("Time elapsed: {:?}", duration);
+    println!("Time elapsed: {duration:?}");
 }
